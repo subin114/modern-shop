e20cafffdb3af94fdd431a893b936d76ccaa505f
subin114 <bin7851@naver.com>
2024-06-14 00:12:49 +0900

    Suspense 추가 (에러 수정 완료)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import { RecoilRoot } from "recoil";
+import { Suspense } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RecoilRoot>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
   </RecoilRoot>
 );
